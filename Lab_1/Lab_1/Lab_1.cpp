7260cdeceb7b1735ffb7993f140855219b0d0310
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-09-15 19:49:59 +0300

    fix: change the render function of the window

--- Lab_1/Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1/Lab_1.cpp
@@ -1,97 +1,76 @@
 #include <Windows.h>
-#include <tchar.h>
-#include <xstring>
 
-#include "resource.h";
+constexpr auto WINDOW_NAME = "Lab_1";
 
-typedef std::basic_string<TCHAR, std::char_traits<TCHAR>,
-	std::allocator<TCHAR> > String;
-
+ATOM RegisterWindowClass(HINSTANCE);
+BOOL InitWindowInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-TCHAR WinName[] = _T("Lab_1");
-HINSTANCE hInst;
-
-int APIENTRY _tWinMain(HINSTANCE This, HINSTANCE Prev, LPTSTR cmd, int mode) 
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR cmdLine, int cmdShowMode)
 {
-	HWND hWnd;    // Window descriptor.
-	MSG msg;      // Structure for storing the message.
-	WNDCLASS wc;  // Window class.
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(cmdLine);
 
-	wc.hInstance = This;                          
-	wc.lpszClassName = WinName;                  // Window name.
-	wc.lpfnWndProc = WndProc;                    // Callback window function.
-	wc.style = CS_HREDRAW | CS_VREDRAW;          // Window style.
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);  // Default window icon.
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);    // Default mouse cursor.
-	wc.lpszMenuName = NULL;                      // Window has no menu.
-	wc.cbClsExtra = 0;                           // No extra class info.
-	wc.cbWndExtra = 0;                           // No extra window info.
+	MSG msg;
 
-	// Fill window with white color.
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	RegisterWindowClass(hInstance);
 
-	hInst = This;
+	if (!InitWindowInstance(hInstance, cmdShowMode))
+		return FALSE;
 
-	// Class registration
-	if (!RegisterClass(&wc))
-		return 0;
-
-	hWnd = CreateWindow(WinName,  // Window name.
-		_T("Lab_1"),              // Window title.
-		WS_OVERLAPPEDWINDOW,      // Window style.
-		CW_USEDEFAULT,            // X.
-		CW_USEDEFAULT,            // Y.
-		CW_USEDEFAULT,            // Window width.
-		CW_USEDEFAULT,            // Window height.
-		HWND_DESKTOP,             // Parent window descriptor.
-		NULL,                     // Window has no menu.
-		This,                     // Application descriptor.
-		NULL);                    // No extra info.
-
-	ShowWindow(hWnd, mode);
-
-	// Message processing cycle.
-	while (GetMessage(&msg, NULL, 0, 0)) {
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
-	return 0;
+	return (int)msg.wParam;
 }
 
-// The window function is called by the operating system and
-// receives messages from the queue for this application.
+ATOM RegisterWindowClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX windowClassEx;
+
+	windowClassEx.cbSize = sizeof(WNDCLASSEX);
+	windowClassEx.style = CS_HREDRAW | CS_VREDRAW;
+	windowClassEx.lpfnWndProc = WndProc;
+	windowClassEx.cbClsExtra = 0;
+	windowClassEx.cbWndExtra = 0;
+	windowClassEx.hInstance = hInstance;
+	windowClassEx.hIcon = LoadIcon(0, IDI_WINLOGO);;
+	windowClassEx.hCursor = LoadCursor(0, IDC_ARROW);
+	windowClassEx.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowClassEx.lpszMenuName = 0;
+	windowClassEx.lpszClassName = WINDOW_NAME;
+	windowClassEx.hIconSm = 0;
+
+	return RegisterClassEx(&windowClassEx);
+}
+
+BOOL InitWindowInstance(HINSTANCE hInstance, int cmdShowMode)
+{
+	HWND hWnd;
+
+	hWnd = CreateWindow(WINDOW_NAME, WINDOW_NAME, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+	if (!hWnd)
+		return FALSE;
+
+	ShowWindow(hWnd, cmdShowMode);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	PAINTSTRUCT ps;
-	HDC hdc;
-	HBITMAP hBitmap;
-
-	static HDC memBitMap;
-	static BITMAP bm;
-
 	switch (message)
 	{
-	case WM_CREATE:
-		hBitmap = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP1));
-		GetObject(hBitmap, sizeof(bm), &bm);
-		hdc = GetDC(hWnd);
-		memBitMap = CreateCompatibleDC(hdc);
-		SelectObject(memBitMap, hBitmap);
-		ReleaseDC(hWnd, hdc);
-		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		BitBlt(hdc, 0, 0, bm.bmWidth, bm.bmHeight, memBitMap, 0, 0, SRCCOPY);
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY: 
+	case WM_DESTROY:
 		PostQuitMessage(0);
-		break;
+		return 0;
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
-
-	return 0;
 }
